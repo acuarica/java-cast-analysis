d13e46fb0ee975b095196cab73066a3cbdeaaae5
luigi <luismastrangelo@usi.ch>
2017-08-21 18:52:59 +0200

    More on cond

--- samples/Cond.java
+++ samples/Cond.java
@@ -1,10 +1,16 @@
 
 public class Cond {
 
-    public static String method(boolean arg) {
+    public static String method1(boolean arg) {
         Object s = "Ciao";
         Object t = "Hola";
         return (String)(arg ? s : t);
+    }
+
+    public static Number method2(boolean arg) {
+        Object s = 4;
+        Object t = 2.3;
+        return arg ? (Integer)s : (Float)t;
     }
 
 }
